66b2e2158aceb805983d5ed1f3c44801f9f2a806
a.gaeva <a.gaeva@space307.com>
2024-07-02 18:57:02 +0300

    HW2

--- src/main/java/AG/qa/guru/Main.java
+++ src/main/java/AG/qa/guru/Main.java
@@ -2,6 +2,6 @@ package AG.qa.guru;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Test messeage!");
     }
 }
